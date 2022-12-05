5737781d69686864416cd3b075935100518712da
bonface221 <mainaboniface221@gmail.com>
2022-12-05 14:45:38 +0300

    ft:added the product serializers

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'playground',
     'debug_toolbar',
     'store',
@@ -149,3 +150,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK ={
+    'COERCE_DECIMAL_TO_STRING':False
+}
